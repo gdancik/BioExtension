23629407a31ec86dd0493f1ec593db0722e62955
Daniel Shenkle <Daniel Shenkle>
2016-09-06 15:57:22 -0400

    Updated javascript so that words are now highlighted and a tool-tip shows when the user hovers over the word with the mouse.

--- content.js
+++ content.js
@@ -1,31 +1,27 @@
-var html = document.getElementsByTagName('body')[0];
-var text = html.innerHTML;
+
+//Words to check
+var words = ["Democratic", "Jacksonians", "Massachusetts"];
+
+//Definitions of words
+var defs = ["Political Party", "I really don't know", "State in New-England"];
+
+
+
+//Start of program
+
+var text = document.body.innerHTML;
 
 var textSplit = text.split(" ");
 var wordCount = 0;
 for (var i = 0; i < textSplit.length; i++){
-	if (textSplit[i] === 'Democratic'){
-		var string = '<span id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">Democratic </span>';
-		console.log(string);
-		textSplit[i] = '<span id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">Democratic </span>';
-		wordCount++;
+	for (var j = 0; j < words.length; j++){
+		if (textSplit[i] === words[j]){
+			console.log(defs[j]);
+			textSplit[i] = '<span title="' + defs[j] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
+			wordCount++;
+		}
 	}
+	
 }
-html.innerHTML = textSplit.join(" ");
+document.body.innerHTML = textSplit.join(' ');
 
-for (var i = 0; i < wordCount; i++){
-	var idVal = "bioPlugIn" + i;
-	console.log(idVal);
-	document.getElementById(idVal).addEventListener("mouseover", mouseOver);
-	document.getElementById(idVal).addEventListener("mouseout", mouseOut);
-}
-
-
-
-function mouseOver(){
-	document.getElementById("bioPlugIn0").style.color = "red";
-}
-
-function mouseOut(){
-	document.getElementById("bioPlugIn0").style.color = "black";
-}
