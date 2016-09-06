552a3f8c455314613aaca8d596120ab735623ac9
Daniel Shenkle <Daniel Shenkle>
2016-09-06 17:18:53 -0400

    Adding enable/ disable function as well as a new icon.

---
+++ background.js
@@ -0,0 +1,12 @@
+var toggle = false;
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggle = !toggle;
+  if(toggle){
+    //chrome.browserAction.setIcon({path: "on.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {file:"content.js"});
+  }
+  else{
+    //chrome.browserAction.setIcon({path: "off.png", tabId:tab.id});
+    chrome.tabs.executeScript(tab.id, {code:"alert()"});
+  }
+});
--- content.js
+++ content.js
@@ -1,4 +1,7 @@
 
+//Enable or disable 
+var run = true;
+
 //Words to check
 var words = ["Democratic", "Jacksonians", "Massachusetts"];
 
@@ -8,20 +11,32 @@ var defs = ["Political Party", "I really don't know", "State in New-England"];
 
 
 //Start of program
-
-var text = document.body.innerHTML;
-
-var textSplit = text.split(" ");
-var wordCount = 0;
-for (var i = 0; i < textSplit.length; i++){
-	for (var j = 0; j < words.length; j++){
-		if (textSplit[i] === words[j]){
-			console.log(defs[j]);
-			textSplit[i] = '<span title="' + defs[j] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
-			wordCount++;
-		}
-	}
+if (run){
+	var text = document.body.innerHTML;
 	
+	var textSplit = text.split(" ");
+	var wordCount = 0;
+	for (var i = 0; i < textSplit.length; i++){
+		for (var j = 0; j < words.length; j++){
+			if (textSplit[i] === words[j]){
+				console.log(defs[j]);
+				textSplit[i] = '<span title="' + defs[j] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
+				wordCount++;
+			}
+		}
+	document.body.innerHTML = textSplit.join(' ');
+	}
 }
-document.body.innerHTML = textSplit.join(' ');
+
+
+
+function enable(){
+	run = true;
+	alert('enabled');
+}
+
+function disable(){
+	run = false;
+	alert('disabled');
+}
 
