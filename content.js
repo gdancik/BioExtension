5bcaa9118c76c5dac6a60861016321f24d652f78
Daniel Shenkle <Daniel Shenkle>
2016-09-12 10:20:16 -0400

    Git backup of laptop (current files should be on git)

--- content.js
+++ content.js
@@ -11,22 +11,21 @@ var defs = ["Political Party", "I really don't know", "State in New-England"];
 
 
 //Start of program
-if (run){
+
 	var text = document.body.innerHTML;
 	
 	var textSplit = text.split(" ");
 	var wordCount = 0;
 	for (var i = 0; i < textSplit.length; i++){
-		for (var j = 0; j < words.length; j++){
-			if (textSplit[i] === words[j]){
-				console.log(defs[j]);
-				textSplit[i] = '<span title="' + defs[j] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
+		var indexTemp = words.indexOf(textSplit[i]);
+		if (indexTemp > -1){
+			console.log(defs[j]);
+				textSplit[i] = '<span title="' + defs[i] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
 				wordCount++;
-			}
 		}
 	document.body.innerHTML = textSplit.join(' ');
 	}
-}
+
 
 
 
