2a2229a838c7fc4a504188732d7b9017708186c4
Daniel Shenkle <Daniel Shenkle>
2016-09-19 15:17:50 -0400

    Temp commit (laptop code out of date)

--- content.js
+++ content.js
@@ -11,31 +11,57 @@ var defs = ["Political Party", "I really don't know", "State in New-England"];
 
 
 //Start of program
+console.log("Start");
+var body = document.getElementsByTagName("BODY")[0]
+var tempText = body.innerText;
+var tempSplit = tempText.split(" ");
 
-	var text = document.body.innerHTML;
-	
-	var textSplit = text.split(" ");
-	var wordCount = 0;
-	for (var i = 0; i < textSplit.length; i++){
-		var indexTemp = words.indexOf(textSplit[i]);
-		if (indexTemp > -1){
-			console.log(defs[j]);
-				textSplit[i] = '<span title="' + defs[i] + '" id="bioPlugIn' + wordCount + '" style="background-color: #FFFF00">' + words[j] + ' </span>';
-				wordCount++;
-		}
-	document.body.innerHTML = textSplit.join(' ');
+for (var i = 0; tempSplit.length; i++){
+	//var tIndex = words.indexOf(tempSplit[i]);
+	if (tempSplit[i] === "Democratic"){
+		tempSplit[i] = '<span title"' + 'Political' + '" style="background-color: #FFFF00">' + 'Democratic' + ' </span>';
 	}
-
-
-
-
-function enable(){
-	run = true;
-	alert('enabled');
 }
+body.innerHTML = tempSplit.join(" ");
 
-function disable(){
-	run = false;
-	alert('disabled');
-}
+console.log("Done");
 
+//var elementsOnPage = document.getElementsByTagName("*");
+//	
+//for (var i = 0; i < elementsOnPage.length; i++){
+//	console.log("Searching....");
+//	if (true || !elementsOnPage[i].hasChildNodes()){
+//		console.log("No children!!!");
+//		var text = elementsOnPage[i].innerHTML;
+//		console.log(text);
+//		var textSplit = text.split(" ");
+//		var wordCount = 0;
+//		
+//		for (var j = 0; j < textSplit.length; j++){
+//			
+//			var indexTemp = words.indexOf(textSplit[j]);
+//			if (indexTemp > - 1){
+//				console.log("Word found!!!");
+//				console.log(defs[indexTemp]);
+//				textSplit[j] = '<span title"' + defs[indexTemp] + '" style="background-color: #FFFF00">' + words[indexTemp] + ' </span>';
+//				wordCount++;
+//			}
+//		}
+//		elementsOnPage[i].innerHTML = textSplit.join(" ");
+//	}
+//	
+//}
+//
+//console.log("Done");
+//	
+//function enable(){
+//	run = true;
+//	alert('enabled');
+//}
+//
+//function disable(){
+//	run = false;
+//	alert('disabled');
+//}
+//
+//
