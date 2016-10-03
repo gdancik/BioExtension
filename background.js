0dceb34420de92a65afdbe1775564c3791725ae3
Daniel Shenkle <dmshenkle@aol.com>
2016-10-02 21:14:54 -0400

    Slight changes to popup

--- background.js
+++ background.js
@@ -1,14 +1,23 @@
+//Background script is used for managing variables that get saved to chome's storage API
+
+
 //Check to see if the enable variable is defined
 //If it is not define it otherwise do nothing with it.
- chrome.storage.local.get("enable", function(obj){
-    	if (typeof obj["enable"] === undefined){
-    		console.log("setting not enabled!");
-    		chrome.storage.local.set({"enable":"true"},function (){
-    			console.log("Storage Succesful");
-			});
-    	}
-    });
+chrome.storage.local.get("enable", function(obj){
+   	if (typeof obj["enable"] === undefined){
+   		chrome.storage.local.set({"enable":"true"},function (){
+   			console.log("enable var initialized");
+		});
+   	}
+});
 
+chrome.storage.local.get("search", function(obj){
+	if (typeof obj["search"] === undefined){
+		chrome.storage.local.set({"search":""},function(){
+			console.log("search var initialized")
+		});
+	}
+});
 
 //Set some content from background page
 //chrome.storage.local.set({"enable":"true"},function (){
--- popup.js
+++ popup.js
@@ -120,3 +120,4 @@ function submit(){
 		console.log("Saved string variable");
 	});
 }
+
